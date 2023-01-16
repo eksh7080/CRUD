2f1664b0f5b48d7552eb5b69a931d1d4c1e350c4
Lee Dano <eksh7080@gmail.com>
2023-01-16 18:23:28 +0900

    리덕스 툴킷 적용, 리엑트 쿼리 API 호출 적용

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,28 +1,18 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from 'auth/Login';
-import Signup from 'auth/Signup';
-import Landing from 'Landing';
-import Todo from 'Todo';
+import React from 'react';
+import Router from 'Router';
+import { Provider } from 'react-redux';
+import { store } from 'Globalstate/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-    const token = localStorage.getItem('token');
-
     return (
-        <BrowserRouter>
-            <Routes>
-                {token && (
-                    <>
-                        <Route path="/" element={<Landing />}></Route>
-                        <Route path="/todo" element={<Todo />}></Route>
-                    </>
-                )}
-
-                <Route path="/" element={<Landing />}></Route>
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/signup" element={<Signup />}></Route>
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <Router />
+            </Provider>
+        </QueryClientProvider>
     );
 }
 
---
+++ front/src/Router.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from 'auth/Login';
+import Signup from 'auth/Signup';
+import Landing from 'Landing';
+import Todo from 'Todo';
+
+const Router = () => {
+    const token = localStorage.getItem('token');
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                {token && (
+                    <>
+                        <Route path="/" element={<Landing />}></Route>
+                        <Route path="/todo" element={<Todo />}></Route>
+                    </>
+                )}
+
+                <Route path="/" element={<Landing />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/signup" element={<Signup />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Router;

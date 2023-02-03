e95827d6a4870c031ca37cfc89f66f2865f359d0
Lee Dano <eksh7080@gmail.com>
2023-02-03 15:11:46 +0900

    CORS content-type 추가

--- front/src/utils/api.tsx
+++ front/src/utils/api.tsx
@@ -6,7 +6,8 @@ const LOGIN_TOKEN = localStorage.getItem('token');
 
 const axiosConfig: AxiosRequestConfig = {
     baseURL: BASE_URL,
-    headers: { Authorization: `Bearer ${LOGIN_TOKEN}` },
+    headers: { Authorization: `Bearer ${LOGIN_TOKEN}`, 'Content-Type': 'application/json' },
 };
 
 export const API = axios.create(axiosConfig);
+// API.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
